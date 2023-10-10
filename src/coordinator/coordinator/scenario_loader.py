dd87ef027bf538577eb0be1dec16be9c55fe4e90
ilangtha <ines.langthallner@risc-software.at>
2023-10-10 13:04:13 +0200

    add functionality to update a generator_model/consumption_model of a scenario

--- src/coordinator/coordinator/scenario_loader.py
+++ src/coordinator/coordinator/scenario_loader.py
@@ -12,11 +12,28 @@ from spa_dat.provider import SocketProviderFactory
 from spa_dat.socket.mqtt import MqttConfig
 from spa_dat.socket.typedef import SpaMessage, SpaSocket
 
+"""
+This script loads a scenario or updates a generator or consumer model.
+
+If you want to load a whole scenario, you have to call the script with three arguments:
+    1. the name of the script which is called (scenario_loader.py)
+    2. what you want to load/update ("scenario")
+    3. the path of the scenario to load (config.json-file)
+Example call:    python scenario_loader.py scenario path/of/scenario
+
+If you want to update a model to an existing scenario, you have to call the script with four arguments:
+    1. the name of the script which is called (scenario_loader.py)
+    2. which node you want to update (generator_model or consumer_model)
+    3. the uuid of the node (6d1d3eb9-bf54-4dda-b2f9-c86bf200dfc2) - be sure that it is the id from the current loaded config.json scenario-file
+    4. the path of the new model you want to load
+Example call:    python scenario_loader.py generator_model 6d1d3eb9-bf54-4dda-b2f9-c86bf200dfc2 C:\Projects\SPA_Energie_UseCase\EnergyScheduling\EnergyScheduling\src\coordinator\coordinator\models\generator\hgb_south_10kwp
+"""
+
 # create and configure logger
 logger = logging.getLogger(__name__)
 logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
 
-config_file = "config.json"
+config_file = Path(__file__).parent / "config.json"
 config = None
 with open(config_file, "rt", encoding='utf-8') as input_file:
     config = json.load(input_file)
@@ -25,34 +42,27 @@ socket_provider = SocketProviderFactory.from_config(
     SocketConfig(payload_format=PayloadFormat.JSON, socket_config=MqttConfig(host=config["host"], port=config["port"]))
 )
 app_update_scenario = DistributedApplication(socket_provider)
+app_update_model = DistributedApplication(socket_provider)
 
-consumer_model_path = Path("models/consumer")
-generator_model_path = Path("models/generator")
+consumer_model_path = Path(__file__).parent / "models/consumer"
+generator_model_path = Path(__file__).parent / "models/generator"
 
 # config_file = Path("scenarios/ex0_one_consumer/config.json")
 # config_file = Path("scenarios/ex1_three_consumer/config.json")
 # config_file = Path("scenarios/ex2_ten_consumer/config.json")
 
-if len(sys.argv) != 2:
-    logging.error("wrong number of arguments")
-    # sys.exit(1)
-    scenario_file = '.\scenarios\ex1_three_consumer\config.json'
-else:
-    scenario_file = sys.argv[1]
+# global variables to load scenario
 scenario = None
+data_consumer = None
+data_generator = None
+data_network = None
 
-with open(scenario_file, "rt", encoding='utf-8') as input_file:
-    scenario = json.load(input_file)
-
-scenario_uuid = str(uuid.uuid4())
-
-data_consumer = scenario['scenario']['consumer']
-data_generator = scenario['scenario']['generator']
-data_network = scenario['scenario']['network']
+# global variables to update model
+model_type = None   # ["generator" | "consumer"]
+model_info = {}
 
 # update consumer data
 logging.info("update consumer data")
-
 
 #r = requests.post(uri_consumer + "/scenario/" + scenario_uuid, json=data_consumer)
 #print(r.json())
@@ -119,4 +129,47 @@ async def update_scenario(socket: SpaSocket, state):
         )
     )
 
-app_update_scenario.start()
+
+@app_update_model.producer()
+async def update_model(socket: SpaSocket, state):
+    """Loads a new generator or consumer model to the mqtt server"""
+    
+    with open(model_info['path'], "rb") as model_input_file:
+        logging.info("updating...")
+        model_content = model_input_file.read()
+        await socket.publish(
+            SpaMessage(
+                content_type = 'application/json',
+                payload = json.dumps({ 'file': base64.b64encode(model_content).decode('ascii') }),
+                topic = f"{model_type}/{model_info['uuid']}/model"
+            )
+        )
+        
+# uncomment for debugging:
+# sys.argv = ["scenario_loader.py", "generator_model", "6d1d3eb9-bf54-4dda-b2f9-c86bf200dfc2", "C:\\Projects\\SPA_Energie_UseCase\\EnergyScheduling\\EnergyScheduling\\src\\coordinator\\coordinator\\models\\generator\\Hagenberg_peakpower10_angle22_aspect0"]
+
+if __name__ == '__main__':
+    # check length of arguments
+    print(len(sys.argv))
+    if len(sys.argv) == 3:
+        logging.info("update scenario")
+        if sys.argv[1] == "scenario":
+            scenario_file = sys.argv[2]
+            with open(scenario_file, "rt", encoding='utf-8') as input_file:
+                scenario = json.load(input_file)
+            scenario_uuid = str(uuid.uuid4())            
+            data_consumer = scenario['scenario']['consumer']
+            data_generator = scenario['scenario']['generator']
+            data_network = scenario['scenario']['network']
+            app_update_scenario.start()
+    elif len(sys.argv) == 4:
+        logging.info("update model")
+        if sys.argv[1] == "generator_model":
+            model_type = "generator"
+        elif sys.argv[1] == "consumer_model":
+            model_type = "consumer"
+        model_info["uuid"] = sys.argv[2]
+        model_info["path"] = sys.argv[3]
+        app_update_model.start()
+    else:
+        logging.error("Wrong number of arguments")
