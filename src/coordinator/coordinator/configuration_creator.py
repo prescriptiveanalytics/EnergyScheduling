f8d98ada6169c37b85916ffd4fd89fbb894afd53
ilangtha <ines.langthallner@risc-software.at>
2023-10-10 13:06:22 +0200

    integrate configuration_creator

--- src/coordinator/coordinator/configuration_creator.py
+++ src/coordinator/coordinator/configuration_creator.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import json
-from csv_data_creator import CsvDataCreator
+import uuid
 
 class ConfigurationCreator:
-    def __init__(self, path_grid_connection, path_household, path_generation):
-        self.path_grid_connection = path_grid_connection
-        self.path_household = path_household
-        self.path_generation = path_generation
-        
+    def __init__(self, df_grid_connection, df_household, df_generation):
+        self.df_grid = df_grid_connection
+        self.df_hh = df_household
+        self.df_gen = df_generation
 
     def __get_json(self):
         # set architecture of json
@@ -42,7 +41,7 @@ class ConfigurationCreator:
             "level": 7,
             "type": "generator",
             "category": "household",
-            "profile_identifier": "pvgis_hgp_south_10kwp"
+            "profile_identifier": "Hagenberg_peakpower10_angle22_aspect0"   # TODO: model in main setzen und übergeben!!!
         }
         
     def __set_attr_entities(self):
@@ -79,15 +78,14 @@ class ConfigurationCreator:
             "length_km": 0.1
         }
 
-
     def generate_config(self):
         json_str = self.__get_json()
+        
         # households:
-        df_hh = pd.read_csv(self.path_household, sep=";")
         # convert each household entry to a dict
-        hh_dict = pd.DataFrame.to_dict(df_hh, orient='records')
+        hh_dict = pd.DataFrame.to_dict(self.df_hh, orient='records')
         # if attributes are not set
-        if len(df_hh.columns) < 9:
+        if len(self.df_hh.columns) < 9:
             # merge each dict with set attributes
             for dict in hh_dict:
                 dict.update(self.__set_attr_households())
@@ -95,11 +93,10 @@ class ConfigurationCreator:
         json_str["scenario"]["consumer"]["consumers"] = hh_dict
         
         # generator:
-        df_gen = pd.read_csv(self.path_generation, sep=";")
         # convert each entry to a dict
-        gen_dict = pd.DataFrame.to_dict(df_gen, orient='records')
+        gen_dict = pd.DataFrame.to_dict(self.df_gen, orient='records')
         # if attributes are not set
-        if len(df_gen.columns) < 9:
+        if len(self.df_gen.columns) < 9:
             # merge each dict with set_attr
             for dict in gen_dict:
                 dict.update(self.__set_attr_generator())
@@ -107,10 +104,9 @@ class ConfigurationCreator:
         json_str["scenario"]["generator"]["generators"] = gen_dict
         
         # grid connections - entities:
-        df_grid = pd.read_csv(self.path_grid_connection, sep=";")
         # convert each entry to a dict
-        ent_dict = pd.DataFrame.to_dict(df_grid, orient='records')
-        if len(df_grid.columns) < 8:
+        ent_dict = pd.DataFrame.to_dict(self.df_grid, orient='records')
+        if len(self.df_grid.columns) < 8:
             # merge dict with set_attr
             for dict in ent_dict:
                 dict.update(self.__set_attr_entities())
@@ -118,9 +114,9 @@ class ConfigurationCreator:
         json_str["scenario"]["network"]["network"]["entities"] = ent_dict
         
         # get ids
-        id_hh = df_hh.identifier.tolist()
-        id_grid = df_grid.identifier.tolist()
-        id_gen = df_gen.identifier.tolist()
+        id_hh = self.df_hh.identifier.tolist()
+        id_grid = self.df_grid.identifier.tolist()
+        id_gen = self.df_gen.identifier.tolist()
         # add buses
         buses = self.__get_buses(id_hh, id_grid, id_gen)
         # append to json string
@@ -131,7 +127,6 @@ class ConfigurationCreator:
         # append list to json string
         json_str["scenario"]["network"]["network"]["lines"] = lines
         return json_str
-        
         
     def __get_buses(self, id_hh, id_grid, id_gen):
         buses = []
@@ -152,7 +147,6 @@ class ConfigurationCreator:
             buses.append(gen_dict)
         return buses
 
-
     def __get_lines(self, all_ids):
         lines = []
         # create dicts with "from_bus", "to_bus" and set_attr_lines
@@ -163,30 +157,158 @@ class ConfigurationCreator:
             # append dict to list
             lines.append(line_dict)
         return lines
+    
+class DataFrameCreator:
+    def __init__(self, num_consumer, num_generators, num_grid_connections, generator_model, consumer_model, lat=48.3787, lon=14.5173):
+        self.num_consumer = num_consumer
+        self.num_generators = num_generators
+        self.num_grid_connections = num_grid_connections
+        self.address_number = 0
+        self.lat = lat
+        self.lon = lon
+        
+    def to_csv(self, filename, data):
+        with open(file=filename, mode="w") as outfile:
+            outfile.write(data)
+        
+    def create_df_grid_connection(self):
+        df_grid_connection = pd.DataFrame()
+        name = []
+        identifier = []
+        latitude = []
+        longitude = []
+        address = []
+        types = []
+        category = []
+        network_entity = []
 
- 
+        for c in range(1, self.num_grid_connections+1):
+            name.append("grid connection " + str(c))
+            identifier.append(str(uuid.uuid4()))
+            latitude.append(round(self.lat, 4))
+            longitude.append(round(self.lon, 4))
+            address.append("Risc Strasse " + str(self.address_number))
+            types.append("network")
+            category.append("network")
+            network_entity.append("network")
+            # increment location
+            self.lat += 0.0002
+            self.lon += 0.0002
+            self.address_number += 1
+            
+        # create dataframe
+        df_grid_connection["name"] = name
+        df_grid_connection["identifier"] = identifier
+        df_grid_connection["latitude"] = latitude
+        df_grid_connection["longitude"] = longitude
+        df_grid_connection["address"] = address
+        df_grid_connection["type"] = types
+        df_grid_connection["category"] = category
+        df_grid_connection["network_entity"] = network_entity
+        
+        return df_grid_connection
+    
+    
+    def create_df_household(self):
+        df_consumer = pd.DataFrame()
+        name = []
+        identifier = []
+        latitude = []
+        longitude = []
+        address = []
+        level = []
+        types = []
+        category = []
+        profile_identifier = []
+
+        for c in range(1, self.num_consumer+1):
+            name.append("Household " + str(c))
+            identifier.append(str(uuid.uuid4()))
+            latitude.append(round(self.lat, 4))
+            longitude.append(round(self.lon, 4))
+            address.append("Risc Strasse " + str(self.address_number))
+            level.append(7)
+            types.append("load")
+            category.append("household")
+            profile_identifier.append(consumer_model)
+            # increment lat and lon
+            self.lat += 0.0002
+            self.lon += 0.0002
+            self.address_number += 1
+            
+        # create dataframe
+        df_consumer["name"] = name
+        df_consumer["identifier"] = identifier
+        df_consumer["latitude"] = latitude
+        df_consumer["longitude"] = longitude
+        df_consumer["address"] = address
+        df_consumer["level"] = level
+        df_consumer["type"] = types
+        df_consumer["category"] = category
+        df_consumer["profile_identifier"] = profile_identifier
+        
+        return df_consumer
+    
+
+    def create_df_generator(self):
+        df_generator = pd.DataFrame()
+        name = []
+        identifier = []
+        latitude = []
+        longitude = []
+        address = []
+        level = []
+        types = []
+        category = []
+        profile_identifier = []
+
+        for c in range(1, self.num_generators+1):
+            name.append("photovoltaic " + str(c))
+            identifier.append(str(uuid.uuid4()))
+            latitude.append(round(self.lat, 4))
+            longitude.append(round(self.lon, 4))
+            address.append("Risc Strasse " + str(self.address_number))
+            level.append(7)
+            types.append("generator")
+            category.append("household")
+            profile_identifier.append(generator_model)
+            # increment lat and lon
+            self.lat += 0.0002
+            self.lon += 0.0002
+            self.address_number += 1
+            
+        # create dataframe
+        df_generator["name"] = name
+        df_generator["identifier"] = identifier
+        df_generator["latitude"] = latitude
+        df_generator["longitude"] = longitude
+        df_generator["address"] = address
+        df_generator["level"] = level
+        df_generator["type"] = types
+        df_generator["category"] = category
+        df_generator["profile_identifier"] = profile_identifier
+        
+        return df_generator
+
+
 if __name__ == "__main__":
-    # set paths and variables
-    # path_grid_conn = "./energy_data/grid_connections.csv"
-    # path_household = "./energy_data/households.csv"
-    # path_generation = "./energy_data/generators.csv"
-    path_grid_conn = "./grid_connections.csv"
-    path_household = "./households.csv"
-    path_generation = "./generators.csv"
-    consumer = 15
+    # set models and variables
+    generator_model = "hgb_east_10kwp"
+    consumer_model = "london2011-2014_cluster0"
+    consumer = 300
     generators = 1
     grid_conns = 1
     
     # create csv data and save to given paths
-    c = CsvDataCreator(num_consumer=consumer, num_generators=generators, num_grid_connections=grid_conns)    
-    c.create_df_grid_connection(path_grid_conn)
-    c.create_df_household(path_household)
-    c.create_df_generator(path_generation)
+    c = DataFrameCreator(consumer, generators, grid_conns, generator_model, consumer_model)    
+    df_grid = c.create_df_grid_connection()
+    df_hh = c.create_df_household()
+    df_gen = c.create_df_generator()
     
     # create configuration file
-    config_creator = ConfigurationCreator(path_grid_connection=path_grid_conn, path_household=path_household, path_generation=path_generation)
+    config_creator = ConfigurationCreator(df_grid_connection=df_grid, df_household=df_hh, df_generation=df_gen)
     json_str = config_creator.generate_config()
     # save json config
     json_obj = json.dumps(json_str, indent=4)
-    with open("./_config.json", "w") as of:
+    with open(f"scenarios/{consumer}_consumer/config.json", "w") as of:
         of.write(json_obj)
