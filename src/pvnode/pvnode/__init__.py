1884f4f0e7757552780c403b02db535c8a22e3e3
ilangtha <ines.langthallner@risc-software.at>
2024-01-09 14:13:30 +0100

    add configuration_model and PowerPvModel

---
+++ src/pvnode/pvnode/__init__.py
@@ -0,0 +1,1 @@
+"""pvnode package."""
---
+++ src/pvnode/pvnode/domain_models/PowerPvModel.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Json
+from datetime import datetime
+
+class PowerPvModel(BaseModel):
+    identifier: str
+    datetime: datetime
+    usage: int
+    category: str
+    category_unit: str
+    interval: int
+    interval_unit: str
