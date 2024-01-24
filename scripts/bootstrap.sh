ef896aae05cf8bba0d823ef799e653450cd358bd
Michael Boegl <michael.boegl@risc-software.at>
2024-01-24 15:22:52 +0000

    Fix boostrap script for devcontainer

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,13 +1,33 @@
 #!/bin/sh
 
-echo "Starting mosquitto mqtt service"
+DIR=`pwd`
+echo "Working directory $DIR"
 
+echo "Starting mosquitto mqtt service"
 service mosquitto start
 
 echo "Updating Python environments"
 
+cd src/consumer
+poetry install
+cd ../..
+
+cd src/generator
+poetry install
+cd ../..
+
+cd src/coordinator
+poetry install
+cd ../..
+
+cd src/network
+poetry install
+cd ../..
+
 cd src/network_area
 poetry install
 cd ../..
+
+cd src/pvnode
 
 echo "Done"
